27df69cb9d8c587820697787542168740b4e9855
Aakarshashetty <aakarshakodthiwada61121@gmail.com>
2022-10-12 21:36:16 +0530

    added script

--- script/app.js
+++ script/app.js
@@ -1,11 +1,11 @@
 const ratingCard = document.querySelector(".rating-card");
-const li = document.querySelectorAll("li");
+const span = document.querySelectorAll("span");
 const thankYou = document.querySelector(".thank-you");
 const p = document.querySelector(".thank-you p");
 const submit = document.querySelector(".submit");
 thankYou.style.display = "none";
-let rate= 0;
-li.forEach(list=>{
+
+span.forEach(list=>{
     list.addEventListener("click",()=>{
         let user_rate = list.innerHTML;
         p.innerText = `You selected ${user_rate} out of 5`;  
