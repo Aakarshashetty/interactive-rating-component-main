e454db99136874e8856af2cc69d5088b3aa1bcbb
Aakarshashetty <aakarshakodthiwada61121@gmail.com>
2022-10-13 17:24:52 +0530

    added responsive css

--- script/app.js
+++ script/app.js
@@ -3,17 +3,21 @@ const span = document.querySelectorAll(".span");
 const thankYou = document.querySelector(".thank-you");
 const p = document.querySelector(".thank-you p");
 const submit = document.querySelector(".submit");
-const rating = document.querySelector(".rating");
 thankYou.style.display = "none";
-span.disabled = "true";
+
 span.forEach(list=>{
     list.addEventListener("click",()=>{
         let user_rate = list.innerHTML;
         p.innerText = `You selected ${user_rate} out of 5`;  
         list.classList.add("selected");
+        
     });
 });
+
+
 submit.addEventListener("click",()=>{
     ratingCard.style.display = "none";
     thankYou.style.display = "block";
 });
+
+
